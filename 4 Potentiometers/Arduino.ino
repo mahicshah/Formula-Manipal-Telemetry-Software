e5c8f64bd7e60182119fc55c3de3ef059d5167d2
Mahic Shah <82862036+mahicshah@users.noreply.github.com>
2022-02-06 19:46:35 +0530

    Create Arduino.ino

---
+++ 4 Potentiometers/Arduino.ino
@@ -0,0 +1,37 @@
+int value1 = 0;
+int value2 = 0;
+int data1 = 0;
+int data2 = 0;
+int data3 = 0;
+int data4 = 0;
+
+int data1mapped = 0;
+int data2mapped = 0;
+int data3mapped = 0;
+int data4mapped = 0;
+
+int pin1 = A0;
+int pin2 = A1;
+int pin3 = A2;
+int pin4 = A3;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+
+ data1 = analogRead(pin1);
+ data2 = analogRead(pin2);
+ data3 = analogRead(pin3);
+ data4 = analogRead(pin4);
+ 
+ data1mapped = map(data1, 0, 1023, 0, 255);
+ data2mapped = map(data2, 0, 1023, 0, 255);
+ data3mapped = map(data3, 0, 1023, 0, 255);
+ data4mapped = map(data4, 0, 1023, 0, 255);
+
+ uint8_t Potentiometer[4] = {data1mapped, data2mapped, data3mapped, data4mapped};
+ 
+ Serial.write(Potentiometer, 4);
+}
